988ffca542dab070549b5c8ba94c49869a9a98ca
Sanjay <sksanjaychopra8@gmail.com>
2018-11-10 14:13:33 +0530

    remove popup

--- src/app/watchlist/watchlistpage/watchlistpage.component.ts
+++ src/app/watchlist/watchlistpage/watchlistpage.component.ts
@@ -11,6 +11,7 @@ export class WatchlistpageComponent implements OnInit {
   private focused : boolean;
   modalClassName;
   user: any;
+  deletedID: any;
   
   constructor(private watchlistService:WatchlistService,private router: Router) {
     
@@ -44,10 +45,18 @@ export class WatchlistpageComponent implements OnInit {
         let element:HTMLElement = document.getElementById('reload') as HTMLElement;
         element.click();
   }
+
+  openDeleteModal(id){
+    this.deletedID = id;
+    let element:HTMLElement = document.getElementById('opendelete') as HTMLElement;
+        element.click();
+  }
   onRemove(id){
     var removeIndex = this.users.map(function(user) { return user.id; })
                        .indexOf(id);
                       this.users.splice(removeIndex, 1)
+    let element:HTMLElement = document.getElementById('closeDel') as HTMLElement;
+    element.click();
     this.watchlistService.deleteUser(id).subscribe(
       user => 
       this.getUsersList()   
