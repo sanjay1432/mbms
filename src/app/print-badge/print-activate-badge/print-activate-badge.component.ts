582a4d4da36b54904b9ae63bf10c29cbb55fd69c
Sanjay <sksanjaychopra8@gmail.com>
2018-12-25 21:42:42 +0530

    GO screen

--- src/app/print-badge/print-activate-badge/print-activate-badge.component.ts
+++ src/app/print-badge/print-activate-badge/print-activate-badge.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-activate-badge.component.css']
 })
 export class PrintActivateBadgeComponent implements OnInit {
-
+  yesClass = 'yes';
+  noClass  = 'no';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  printBadge(value){
+    if(value  === 'yes'){
+      this.yesClass = value
+      this.noClass = 'no'
+     
+    }else if(value  === 'no'){
+      this.yesClass = value
+      this.noClass = 'yes'
+    
+    }
+    
   }
 
 }
