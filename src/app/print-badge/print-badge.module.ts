970767077d794a6ac8a97cbabe3370c0173fee81
Sanjay <sksanjaychopra8@gmail.com>
2019-01-01 21:59:53 +0530

    print-label screen

--- src/app/print-badge/print-badge.module.ts
+++ src/app/print-badge/print-badge.module.ts
@@ -5,10 +5,16 @@ import { PrintBadgeRoutingModule } from './print-badge-routing.module';
 import { PrintLabelComponent } from './print-label/print-label.component';
 import { PrintBadgeComponent } from './print-badge/print-badge.component';
 import { PrintAssignBadgeComponent } from './print-assign-badge/print-assign-badge.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     CommonModule,
-    PrintBadgeRoutingModule
+    PrintBadgeRoutingModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    HttpClientModule
   ],
   declarations: [PrintActivateBadgeComponent, PrintLabelComponent, PrintBadgeComponent, PrintAssignBadgeComponent]
 })
--- src/app/print-badge/print-label/print-label.component.ts
+++ src/app/print-badge/print-label/print-label.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MannedVisitorMangementService } from '../../services/manned-visitor-mangement.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-print-label',
   templateUrl: './print-label.component.html',
   styleUrls: ['./print-label.component.css']
 })
 export class PrintLabelComponent implements OnInit {
-
-  constructor() { }
+  printers = ['printer1','printer2','printer3']
+  constructor(private mannedVisitorMangementService:MannedVisitorMangementService) { }
 
   ngOnInit() {
+    this.mannedVisitorMangementService.getVisitorProfile()
+    .subscribe((profile)=> 
+     {
+       console.log(profile)
+      //  this.preRegData = profile.preRegisterData,
+      //  this.profileData = profile.profileData
+      //  this.host = profile.host
+      //  this.visitor = profile.visitor
+     })
   }
 
 }
